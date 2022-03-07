571dd5c8945727df1641592cea6579fb19ce2a2a
Eray Alakese <erayalakese@gmail.com>
2022-03-07 09:14:34 +0300

    'init'

---
+++ script.js
@@ -0,0 +1,39 @@
+const code = "var requestOptions = {\n" +
+    "  method: 'GET'\n" +
+    "};\n" +
+    "\n" +
+    "fetch(\"https://api.amazonimagesapi.com/:ASIN:/:domain:\",\n" +
+    "requestOptions)\n" +
+    "  .then(response => response.text())\n" +
+    "  .then(result => console.log(result))\n" +
+    "  .catch(error => console.log('error', error));"
+
+$(document).ready(function () {
+    $(".search").on("click", function () {
+        const asin = $('#asin').val();
+        const domain = $('#domain').val();
+
+        let text = code
+
+        text = text.replace(/:ASIN:/g, asin)
+        text = text.replace(/:domain:/g, domain)
+
+        $("pre.request").get(0).innerText = text
+
+        var requestOptions = {
+            method: 'GET'
+        };
+
+        fetch("https://api.amazonimagesapi.com/"+asin+"/"+domain,
+            requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                $("pre.response").get(0).innerText = result
+                console.log(result)
+            })
+            .catch(error => {
+                $("pre.response").get(0).innerText = error
+                console.log('error', error)
+            });
+    });
+})
